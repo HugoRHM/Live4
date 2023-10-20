0376f57de98d0c05a8a066d9a177dd9d06092859
HugoRHM <72580471+HugoRHM@users.noreply.github.com>
2023-10-20 12:50:14 -0600

    Add files via upload

---
+++ Live4/src/org/generation/live4/Live4.java
@@ -0,0 +1,21 @@
+// Hugo Ricardo Hernández Montejano 
+// Revisado por los Mictlanes
+// Califican: 3 
+
+package org.generation.live4;
+
+public class Live4 {
+	public void imprimir() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ Live4/src/org/generation/live4/Main.java
@@ -0,0 +1,8 @@
+package org.generation.live4;
+
+public class Main {
+	 public static void main(String[] args) {
+	        Live4 fizzBuzz = new Live4();
+	        fizzBuzz.imprimir();
+	    }
+}
